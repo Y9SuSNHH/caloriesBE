4c2d281ec8356364cee7f7ce86c9aedee132d37f
Nguyễn Huy Hùng <y9susnhh@gmail.com>
2023-03-08 18:36:30 +0700

    Update BmiController.php

--- app/Http/Controllers/BmiController.php
+++ app/Http/Controllers/BmiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Bmi;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Validation\Rule;
 
@@ -23,20 +24,23 @@ class BmiController extends Controller
     {
         try {
             $validated = $request->validate([
-                'month' => [
-                    'integer',
-                ],
-                'year'  => [
-                    'integer',
+                'date' => [
+                    'date_format:Y-m-d',
                 ],
             ], [
-                'integer' => ':attribute phải là một số',
+                'date_format' => ':attribute sai định dạng',
             ]);
 
-            $query    = $this->model->clone()->where('user_id', $userId);
-            $data     = $query->get();
-            $bmi_user = $data;
-            return $this->successResponse($data, 'Lấy danh sách bài tập');
+            $query = $this->model->clone()->where('user_id', $userId);
+
+
+            if (!empty($validated['date'])) {
+                $date = new Carbon($validated['date']);
+                $query->whereMonth('created_at', $date->month)->whereYear('created_at', $date->year);
+            }
+
+            $data = $query->get();
+            return $this->successResponse($data, 'Lấy danh sách bài tập thành công');
         } catch (\Throwable $e) {
             return $this->errorResponse($e->getMessage());
         }
